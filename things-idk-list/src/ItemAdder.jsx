5957d3a5dd3d98487634e869b7885adc8e5ac369
wystler <benjamin.rudd@live.com>
2023-01-10 11:19:23 +0000

    move focus back to input box after clicking add item button

--- things-idk-list/src/ItemAdder.jsx
+++ things-idk-list/src/ItemAdder.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 const ItemAdder = (props) => {
 
     const {setItems} = props
     const [newItem, setNewItem] = useState("")
+    const ref = useRef("input")
 
     const handleSubmit = (event) => {
         event.preventDefault()
+        ref.current.focus()
         if (newItem)
         setItems((currentItems) => {
             return [...currentItems, {id:Math.max(...currentItems.map( item => item.id > 0 ? item.id : 0))+1, description:newItem}]
@@ -19,6 +21,7 @@ const ItemAdder = (props) => {
             <label className="itemAdderLabel">
                 Add a new item:
                 <input
+                ref={ref}
                 className="itemAdderBox"
                 value={newItem}
                 onChange={(event) => setNewItem(event.target.value)}>
